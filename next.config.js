0929759277a5eab051a110316f653f83e2864a2d
SHAHULDEV22 <shahuldev22@gmail.com>
2022-07-13 20:04:32 +0530

    setup image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: false,
+    },
+  },
   basePath: '/nextportfolio',
   assetPrefix: '/nextportfolio'
 }
